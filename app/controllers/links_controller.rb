c7496ed4c9ecb04fca3c91cf01b317759f05af15
Kennysaruni <kenny.saruni@student.moringaschool.com>
2024-02-16 12:10:30 +0300

    Added routes for the CRUD (create read update delete) operations

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,5 +1,4 @@
 class LinksController < ApplicationController
-  before_action :set_link, only: %i[ show edit update destroy ]
 
   # GET /links or /links.json
   def index
@@ -17,63 +16,29 @@ class LinksController < ApplicationController
       redirect_to link.url
     end
   end
-  
-
-  # GET /links/new
-  def new
-    @link = Link.new
-  end
-
-  # GET /links/1/edit
-  def edit
-  end
 
   # POST /links or /links.json
   def create
-    @link = Link.new(link_params)
-
-    respond_to do |format|
-      if @link.save
-        format.html { redirect_to link_url(@link), notice: "Link was successfully created." }
-        format.json { render :show, status: :created, location: @link }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /links/1 or /links/1.json
-  def update
-    respond_to do |format|
-      if @link.update(link_params)
-        format.html { redirect_to link_url(@link), notice: "Link was successfully updated." }
-        format.json { render :show, status: :ok, location: @link }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @link.errors, status: :unprocessable_entity }
-      end
-    end
+    link = Link.new(link_params)
+    if link.save
+      render json: {link.short}, status: :created
+    else
+      render json: {errors: link.errors.full_messages}, status: :unprocessable_entity
   end
 
   # DELETE /links/1 or /links/1.json
   def destroy
-    @link.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to links_url, notice: "Link was successfully destroyed." }
-      format.json { head :no_content }
-    end
+    link = Link.find_by(slug: params[:slug])
+    if link.nil
+      render json: {error: 'Link not found'}, status: :404
+    else
+      link.destroy
+      head: no_content
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_link
-      @link = Link.find(params[:id])
-    end
-
     # Only allow a list of trusted parameters through.
     def link_params
-      params.require(:link).permit(:url, :slug, :clicked)
+      params.require(:link).permit(:url, :slug,)
     end
 end

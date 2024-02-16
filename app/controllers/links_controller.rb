6c9971eb9d628e0bc941e843a80298aa1d57534e
Kennysaruni <kenny.saruni@student.moringaschool.com>
2024-02-16 12:40:05 +0300

    changed post route to /s instead of /s/:slug since there is no slug fixed CRSF token authenticity error fixed syntax and logical errors

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,5 @@
 class LinksController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create, :show, :destroy]
 
   # GET /links or /links.json
   def index
@@ -7,13 +8,14 @@ class LinksController < ApplicationController
 
   # GET /links/1 or /links/1.json
   def show
+    link= Link.find_by(slug: params[:slug])
     if link.nil?
       render 'errors/404', status: 404
     else
       # Updates the clicked counter by 1 everytime the link is clicked
-      link.update(clicked: link.clicked + 1) 
+      link.update(clicked: link.clicked.to_i + 1) 
       # Ensure you're redirecting to the full URL including the host
-      redirect_to link.url
+      redirect_to link.url, allow_other_host: true
     end
   end
 
@@ -21,24 +23,26 @@ class LinksController < ApplicationController
   def create
     link = Link.new(link_params)
     if link.save
-      render json: {link.short}, status: :created
+      render json: {short_link: link.short}, status: :created
     else
       render json: {errors: link.errors.full_messages}, status: :unprocessable_entity
+    end
   end
 
   # DELETE /links/1 or /links/1.json
   def destroy
     link = Link.find_by(slug: params[:slug])
     if link.nil
-      render json: {error: 'Link not found'}, status: :404
+      render json: {error: 'Link not found'}, status: :not_found
     else
       link.destroy
-      head: no_content
+      head :no_content
+    end
   end
 
   private
     # Only allow a list of trusted parameters through.
     def link_params
-      params.require(:link).permit(:url, :slug,)
+      params.permit(:url, :slug,)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # Route for creating a new link
-  post '/s/:slug', to: 'links#create'
+  post '/s', to: 'links#create'
 
   # Route for accesing a link that already exists
   get '/s/:slug', to: 'links#show', as: :short
 
   # Route for deleting an elready existing link
-  delete '/s/slug', to: 'links#destroy'
+  delete '/s/:slug', to: 'links#destroy'
 end
